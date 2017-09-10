0a7004fa2cf23eafd2c6fe608baa9c764f856deb
AlexRoschyn <samuray_6@ukr.net>
2017-09-10 23:22:02 +0300

    Создал вариант3 (иной), и сделал все красиво (отступы, точки, и исправил ошибки.)!)))

--- Module2.java
+++ Module2.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Module2 {
@@ -12,11 +13,11 @@ public class Module2 {
         String age = in.nextLine();
         System.out.println("Введите ваше хобби:");
         String hobby = in.nextLine();
-        System.out.println("Имя:"+name);
-        System.out.println("Город:"+city);
-        System.out.println("Возраст:"+age);
-        System.out.println("Хобби:"+hobby);
-        
+        System.out.println("Имя: "+name);
+        System.out.println("Город: "+city);
+        System.out.println("Возраст: "+age);
+        System.out.println("Хобби: "+hobby);
+
         //Вариант 2 (текстовый):
         System.out.println("Человек по имени:");
         String name1 = in.nextLine();
@@ -26,8 +27,22 @@ public class Module2 {
         String age1 = in.nextLine();
         System.out.println("Он любит заниматься:");
         String hobby1 = in.nextLine();
-        System.out.println("Человек по имени:"+name1+" Живет в городе:"+city1);
-        System.out.println("Этому человеку:"+age1+"лет"+" Он любит заниматься:"+hobby1);
+        System.out.println("Человек по имени "+name1+" живет в городе "+city1+".");
+        System.out.println("Этому человеку "+age1+" лет"+" и любит он заниматься "+hobby1+".");
+
+        //Вариант 3 (иной):
+        System.out.println("Введите имя:");
+        String name2 = in.nextLine();
+        System.out.println("Введите город проживания");
+        String city2 = in.nextLine();
+        System.out.println("Введите ваш возраст");
+        String age2 = in.nextLine();
+        System.out.println("Введите ваше хобби");
+        String hobby2 = in.nextLine();
+        System.out.println(name2+" - имя");
+        System.out.println(city2+" - город");
+        System.out.println(age2+" - возраст");
+        System.out.println(hobby2+" - хобби");
     }
 }
 
